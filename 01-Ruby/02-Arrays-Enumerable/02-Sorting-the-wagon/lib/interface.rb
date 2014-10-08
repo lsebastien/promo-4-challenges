915ad1a04c7c4c9809d2c23cfeed30579232c6e5
Sebastien Leopold <lsebastien@gmail.com>
2014-10-08 15:55:37 +0200

    avec minor changes

--- 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/interface.rb
+++ 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/interface.rb
@@ -11,13 +11,15 @@ require_relative "wagon_sort"
 #       file and display the sorted student list
 nom = nil
 students = []
+
 until nom == ""
   print "type a student name (or press enter to finish)"
   nom = gets.chomp
   students << nom
 end
 
-wagon_sort(students)
-
+students_final_list = wagon_sort(students)
 puts "Congratulations! Your Wagon has #{students.count- 1} students :"
-puts students[0..100]
+
+#students_final_list.map do |student|
+  puts " #{students_final_list[1]}, #{students_final_list[2]} and #{students_final_list[3]}"
